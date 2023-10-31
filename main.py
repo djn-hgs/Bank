0f9c973b5eb716db6a454f55bd5c5d63bead8d41
Daryl Noyce <33289004+djn-hgs@users.noreply.github.com>
2023-10-31 21:28:15 +0000

    Setting things up neatly using @dataclass

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import datetime
+from dataclasses import dataclass
+
+
+@dataclass
 class Person:
-    ...
+    first_name: str
+    last_name: str
+    sex: str
+    date_of_birth: datetime.date
 
 
+@dataclass
 class BankAccount:
-    ...
+    account_number: int
+    owners: [Person]
+    balance: float
 
 
+@dataclass
 class Bank:
-    ...
+    accounts: [BankAccount]
+    log: {datetime.datetime: str}
+
+
 
 
